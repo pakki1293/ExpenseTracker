a8bbc87155893dadfec603da5c577a211141b0d7
Pakki Prasanna <pakki.prasanna@ibm.com>
2023-02-22 21:27:45 +0530

    day-3changes

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
 import { Transaction } from './Transaction';
 
+import { GlobalContext } from '../context/GlobalState';
+
 export const TransactionList = () => {
-    const { transactions } = useContext(GlobalContext);
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id}
-         transaction={transaction} />))}
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
     </>
   )
